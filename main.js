0ebb9d56bc19703a1a7c15ef929dae14961f3c79
Geronimo saldaña <darcaromega@gmail.com>
2020-06-24 21:35:46 -0500

    primer avance en el chat tipo telegram

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(router);
 
-app.use('/',function (req, res){
-    res.send('holaaaaa otra vez');
-})
+router.get('/messege', function (req, res) {
+  console.log(req.headers);
+  res.send("lista de mensajes");
+  res.header({
+        "custom-header": "Valor personalizado"
+  });
+});
+
+/*
+router.get('/messege', function (res, req){
+    console.log(req.headers);  
+    res.send('lista de mensajes');
+});
+*/
+router.delete('/messege', function(req, res){
+    console.log(req.query);
+    console.log(req.body);
+    res.send('Mensaje añadido correctamente');
+});
+
+
 
 app.listen(3000);
-console.log('tu aplicacion esta corriendo');
+console.log('la aplicacion inicio en el puerto 3000')
